986e4b615731072632548dcfa164644ba7a6193a
ademr0 <hi@iamade.me>
2023-01-01 23:05:33 +0700

    backup

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,58 @@
-// Copyright (c) 2022 Ade M Ramdani
-//
-// This file is part of the dal.
-// See the LICENSE file for licensing information.
-
 use dal::{
-    codegen::analyze::ImportTableEntry,
-    compiler::{
-        parser::parse,
-        tokenizer::{tokenize, Tokenization},
-    },
-    ErrColor,
+    lexer::{self, print_tokens},
+    parser,
 };
 
-fn usage(arg0: &str, exit_code: i32) -> ! {
-    println!("Usage: {} [command] [options]", arg0);
+fn usage(cmd: &str, exit_code: i32) -> ! {
+    println!("Usage: {} <command> [options]", cmd);
     println!("Commands:");
-    println!("  build\t\tCompile the project");
-    println!("  version\tShow the version");
-    println!("  help\t\tShow this help");
-    println!("Command: build target");
-    println!("  --release\t\tCompile the project in release mode");
-    println!("  --static\t\tThe project will be statically linked");
-    println!("  --strip\t\tStrip the binary");
-    println!("  --color [auto|on|off]\tColorize the output");
-
+    println!("\tversion\t\tPrint version information");
     std::process::exit(exit_code);
 }
 
 fn version() -> ! {
-    println!("dal 0.1.0");
+    println!("DAL: v0.1.0");
     std::process::exit(0);
 }
 
-fn build(src_path: &str) -> ! {
-    let src = std::fs::read_to_string(src_path).unwrap();
-    let mut tok_out = Tokenization::new();
-    tokenize(&src, &mut tok_out);
-    if tok_out.err.is_some() {
-        println!("Error: {}", tok_out.err.unwrap());
+fn build(args: Vec<String>) -> ! {
+    if args.len() == 0 {
+        println!("No input files");
         std::process::exit(1);
     }
 
-    let mut import_table_enty = ImportTableEntry::new(src_path, &src, tok_out.line_offsets);
-    let ast = parse(&src, tok_out.tokens, &mut import_table_enty, ErrColor::Auto);
-    ast.print_ast(0);
-    // println!("{:#?}", ast);
+    let f = std::fs::read_to_string(&args[0]);
+    if f.is_err() {
+        println!("Failed to read file: {}", args[0]);
+        std::process::exit(1);
+    }
+
+    let src = f.unwrap();
+
+    let mut out = lexer::Tokenization::new();
+    lexer::tokenize(&src, &mut out);
+    if out.errors.is_some() {
+        println!("Failed to tokenize file: {}", args[0]);
+        std::process::exit(1);
+    }
+    print_tokens(&src, &out.tokens);
+
+    let ast = parser::parse(&src, out.tokens.clone());
+    println!("{}", ast);
+
     std::process::exit(0);
 }
 
-fn main() -> ! {
+fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         usage(&args[0], 1);
     }
 
-    match args[1].as_str() {
-        "build" => {
-            let mut target = None;
-            if args.len() > 2 {
-                target = Some(args[2].clone());
-            }
-            if target.is_none() {
-                usage(&args[0], 1);
-            }
-            build(&target.unwrap());
-        }
-        "version" => version(),
-        "help" => usage(&args[0], 0),
+    let cmd = &args[1];
+    match cmd.as_str() {
+        "version" | "-v" | "--version" => version(),
+        "build" => build(args[2..].to_vec()),
         _ => usage(&args[0], 1),
     }
 }
