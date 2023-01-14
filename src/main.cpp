e7e3669af41617c90d3c3f095d99bbc8de9135fb
ademr0 <hi@iamade.me>
2023-01-14 19:51:04 +0700

    project template

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023 Ade M Ramdani
+ *
+ * This file is part of dal, which is MIT licensed.
+ * See the file "LICENSE" for more information or visit
+ * https://opensource.org/licenses/MIT for full license details.
+ */
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
